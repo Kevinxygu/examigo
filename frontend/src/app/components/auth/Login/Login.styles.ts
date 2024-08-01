b648dd3fabec513302a914dca7ca8a59f483f17b
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-07-31 22:37:54 -0700

    working on auth

---
+++ frontend/src/app/components/auth/Login/Login.styles.ts
@@ -0,0 +1,2 @@
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
---
+++ frontend/src/app/components/auth/authInit.ts
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const FIREBASE_KEY = process.env.VITE_FIREBASE_KEY;
+const AUTH_DOMAIN = process.env.VITE_AUTH_DOMAIN;
+const PROJECT_ID = process.env.VITE_PROJECT_ID;
+const STORAGE_BUCKET = process.env.VITE_STORAGE_BUCKET;
+const MESSAGING_ID = process.env.VITE_MESSAGING_ID;
+const APP_ID = process.env.VITE_APP_ID;
+const MEASUREMENT_ID = process.env.VITE_MEASUREMENT_ID;
+
+const firebaseConfig = {
+  apiKey: FIREBASE_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export { app };
