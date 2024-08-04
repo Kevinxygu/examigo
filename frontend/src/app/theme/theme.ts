cc58694aeb70f3c75090dd07134364d34412cd07
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-08-03 17:49:59 -0700

    created theme TS file

---
+++ frontend/src/app/theme/theme.ts
@@ -0,0 +1,25 @@
+import { Linden_Hill } from "next/font/google";
+
+export const colors = {
+    // light blue
+    lightblue: "#A8B7D0",
+
+    // light yellow
+    lightyellow: "#DDBA8D",
+
+    // off white
+    offWhite: "#EAEAEA",
+
+    // dark blue (background)
+    darkblue: "#1C1E22",
+
+    // grey blue
+    greyblue: "#596175",
+
+    // bright blue
+    brightBlue: "#263960",
+
+    // mid blue
+    midBlue: "#2D3142",
+
+}
