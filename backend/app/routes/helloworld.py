ca2b6beac0dbd50473f5a30fabbfee2d7b8db2af
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-08-03 22:47:20 -0700

    updated backend

--- backend/app/routes/helloworld.py
+++ backend/app/routes/helloworld.py
@@ -4,4 +4,5 @@ bp = Blueprint('helloworld', __name__, url_prefix='/helloworld')
 
 @bp.route('/', methods=['GET'])
 def hello_world_ping_route():
-    return jsonify(message="Hello World from Flask!"), 200
+    if request.method == 'GET':
+        return jsonify(message="Hello World from Flask!"), 200
