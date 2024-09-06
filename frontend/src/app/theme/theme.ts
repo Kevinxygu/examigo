9c29c847675ee06b1ed7483fb00525c25a45ce4b
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-09-05 23:24:20 -0700

    caps

--- frontend/src/app/theme/theme.ts
+++ frontend/src/app/theme/theme.ts
@@ -1,18 +1,18 @@
 export const colors = {
     // light blue
-    lightblue: "#A8B7D0",
+    lightBlue: "#A8B7D0",
 
     // light yellow
-    lightyellow: "#DDBA8D",
+    lightYellow: "#DDBA8D",
 
     // off white
     offWhite: "#EAEAEA",
 
     // dark blue (background)
-    darkblue: "#1C1E22",
+    darkBlue: "#1C1E22",
 
     // grey blue
-    greyblue: "#596175",
+    greyBlue: "#596175",
 
     // bright blue
     brightBlue: "#263960",
